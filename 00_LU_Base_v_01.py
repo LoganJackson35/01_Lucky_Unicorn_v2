e5e4d2f453116cace6338596b773c8f8f6713933
LoganJackson35 <78570988+LoganJackson35@users.noreply.github.com>
2021-02-25 13:14:37 +1300

    Starting Token Generator

--- 00_LU_Base_v_01.py
+++ 00_LU_Base_v_01.py
@@ -26,11 +26,37 @@ def instructions():
     return ""
 
 
+def num_check(question, low, high):
+    error = "Please enter an whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # ask the question
+            response = int(input(question))
+            # if the amount is too low / too high give
+            if low < response <= high:
+                return response
+
+            # output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # Main routine goes here...
-played_before = yes_no("Have you played the "
-                           "game before? ")
+played_before = yes_no("Have you played the game before? ")
 
 if played_before == "no":
     instructions()
 
-print("program continues")
+print()
+
+# Ask user how much the want to play with
+
+how_much = num_check("How much would you "
+                     "like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
